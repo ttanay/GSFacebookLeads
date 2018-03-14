b1283b1a1fbe29d29e298d8a11944b7d5cda00f9
ttanay <ttanay100@gmail.com>
2018-03-14 14:37:50 +0530

    more command added

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 python3 -m venv src/env
 source src/env/bin/activate
 pip3 install -r requirements.txt
+deactivate
 sudo echo "alias fbscript='source src/env/bin/activate;python3 src/main.py;'" >> ~/.bashrc
+exit
