a8f0a72c944b512461c0cabe626eb64b4c2afa8a
ttanay <ttanay100@gmail.com>
2018-03-14 14:39:28 +0530

    bug fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 python3 -m venv src/env
 source src/env/bin/activate
 pip3 install -r requirements.txt
-deactivate
 sudo echo "alias fbscript='source src/env/bin/activate;python3 src/main.py;'" >> ~/.bashrc
-exit
