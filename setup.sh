b9f6b9643c5824f8c36eb541736eb9a0cd8e21a7
T. Tanay <ttanay100@gmail.com>
2018-03-14 14:55:08 +0530

    spellings

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ python3 -m venv src/env
 source src/env/bin/activate
 pip3 install -r requirements.txt
 sudo echo "alias fbscript='source src/env/bin/activate;python3 src/main.py;'" >> ~/.bashrc
-echo "Seup Done!"
+echo "Setup Done!"
